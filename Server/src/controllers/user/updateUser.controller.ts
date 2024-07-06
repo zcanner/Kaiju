ad46acb090665bdbaedd3addc05abcd3b8a5acd4
Vivek Tiwari <sudovivek4@gmail.com>
2024-07-06 16:03:24 +0530

    chore: Update updateUser controller to handle user updates

--- Server/src/controllers/user/updateUser.controller.ts
+++ Server/src/controllers/user/updateUser.controller.ts
@@ -1,20 +1,27 @@
 import { Request, Response } from "express";
+import User from "../../schemas/user.schema.js";
 
-const updateUser = (req: Request, res: Response) => {
+const updateUser = async (req: Request, res: Response) => {
   try {
     // Retrieve user ID from request parameters
-    const userId = req.params.id;
-    console.log(res.locals.user);
+    const userId = res.locals.user._id;
     // Retrieve updated user data from request body
     const updatedUserData = req.body;
 
-    // TODO: Implement logic to update user in the database
+    // find user in the data base and update it
 
-    // TODO: Return success response
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update user data
+    await user.updateOne(updatedUserData);
     res.status(200).json({ message: "User updated successfully" });
   } catch (error) {
-    // TODO: Handle error appropriately
-    res.status(500).json({ error: "Internal server error" });
+    const errorMessage =
+      error instanceof Error ? error.message : "Internal server error";
+    res.status(500).json({ error: errorMessage });
   }
 };
 
