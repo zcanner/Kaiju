f1bbccde2634d37100e44f6581e94cafcd7852e7
Vivek Tiwari <vivektiwari@tuta.io>
2024-09-04 18:05:52 +0530

    feat: Update active tab styling on home page
    This commit updates the styling of the active tab on the home page. It changes the position to sticky.

--- client/src/pages/app/home.page.tsx
+++ client/src/pages/app/home.page.tsx
@@ -1,45 +1,43 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import CreatePost from "../../components/home/createpost.component";
-import Posts from "../../components/posts/posts.component";
+import CreatePost from '../../components/home/createpost.component';
+import Posts from '../../components/posts/posts.component';
 
-import useUser from "../../lib/hooks/query/getUser";
-import usePosts from "../../lib/hooks/query/usePosts";
+import useUser from '../../lib/hooks/query/getUser';
+import usePosts from '../../lib/hooks/query/usePosts';
 
 const Home = () => {
   const { data } = useUser();
   // TODO : merge with usePost hook
-  const [active, setActive] = useState("For You");
+  const [active, setActive] = useState('For You');
   const { data: post, refetch } = usePosts(active);
 
-  let isActive = active === "For You";
+  let isActive = active === 'For You';
 
   useEffect(() => {
     refetch();
   }, [active]);
 
   return (
-    <div className="w-full max-w-xl">
-      <div className="w-full max-w-xl items-center content-center border-ghostbg border-b cursor-pointer">
-        <div className="flex gap-2">
+    <div className='w-full max-w-xl'>
+      <div className='w-full sticky top-0 bg-primarybg select-none z-10 max-w-xl items-center content-center border-ghostbg border-b cursor-pointer'>
+        <div className='flex gap-2'>
           <div
-            onClick={() => setActive("For You")}
+            onClick={() => setActive('For You')}
             className={`text-center w-full hover:bg-ghostbg h-14 content-center active:bg-ghostbg ${
-              isActive ? "border-b border-primary text-white" : "text-gray-300"
-            }`}
-          >
-            <div className="items-center">
-              <h1 className="text-lg font-medium p-2">For You</h1>
+              isActive ? 'border-b border-primary text-white' : 'text-gray-300'
+            }`}>
+            <div className='items-center'>
+              <h1 className='text-lg font-medium p-2'>For You</h1>
             </div>
           </div>
           <div
-            onClick={() => setActive("following")}
+            onClick={() => setActive('following')}
             className={`text-center w-full hover:bg-ghostbg h-14 content-center active:bg-ghostbg ${
-              !isActive ? "border-b border-primary text-white" : "text-gray-300"
-            }`}
-          >
-            <div className="items-center">
-              <h1 className="text-lg font-medium p-2">Following</h1>
+              !isActive ? 'border-b border-primary text-white' : 'text-gray-300'
+            }`}>
+            <div className='items-center'>
+              <h1 className='text-lg font-medium p-2'>Following</h1>
             </div>
           </div>
         </div>
